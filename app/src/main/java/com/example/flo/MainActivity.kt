43982cdf3989d9eccc12145dab1941d0cf950ecb
nahy-512 <hedghog1004@naver.com>
2023-09-30 10:58:30 +0900

    ✨ Feat: Week2 - 화면 이동 데이터 전달 구현

--- app/src/main/java/com/example/flo/MainActivity.kt
+++ app/src/main/java/com/example/flo/MainActivity.kt
@@ -1,23 +1,51 @@
 package com.example.flo
 
+import android.app.Activity
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
 import com.example.flo.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+    companion object { const val STRING_INTENT_KEY = "song_key" }
+
     lateinit var binding: ActivityMainBinding
+
+    private val getResultText = registerForActivityResult(
+        ActivityResultContracts.StartActivityForResult()
+    ) {result ->
+        if (result.resultCode == Activity.RESULT_OK) {
+            val returnSong = result.data?.getStringExtra(STRING_INTENT_KEY)
+            // 받아온 앨범 제목으로 토스트 메시지 띄우기
+            Toast.makeText(this, returnSong, Toast.LENGTH_SHORT).show()
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.mainPlayerCl.setOnClickListener {
-            startActivity(Intent(this, SongActivity::class.java))
-        }
+        moveToSongActivity()
         initBottomNavigation()
+    }
+
+    private fun moveToSongActivity() {
+        val song = Song(binding.mainMiniplayerTitleTv.text.toString(), binding.mainMiniplayerSingerTv.text.toString())
+        Log.d("Song", song.title + song.singer)
+
+        binding.mainPlayerCl.setOnClickListener {
+            val intent = Intent(this, SongActivity::class.java)
+            intent.putExtra("title", song.title)
+            intent.putExtra("singer", song.singer)
+//            startActivity(intent)
+            // SongActivity에서 돌아올 때 받은 앨범 제목
+            getResultText.launch(intent)
+        }
     }
 
     private fun initBottomNavigation(){
