e4bf4233212d8f65442c1a31529de0d766f7cc89
nahy-512 <hedghog1004@naver.com>
2023-10-07 17:48:05 +0900

    ✨ Feat: Week4 - Thread를 활용한 노래 재생 Timer Seekbar 구현

--- app/src/main/java/com/example/flo/Song.kt
+++ app/src/main/java/com/example/flo/Song.kt
@@ -2,5 +2,8 @@ package com.example.flo
 
 data class Song(
     val title: String = "",
-    val singer: String = ""
+    val singer: String = "",
+    val second: Int = 0,
+    val playTime: Int = 0,
+    var isPlaying: Boolean = false
 )
