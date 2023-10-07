0616127e4fc4fe323df2fc8111ed877e162a08af
nahy-512 <hedghog1004@naver.com>
2023-10-07 19:20:49 +0900

    Feat: Week4 - Theme을 이용한 Splash 구현

--- app/src/main/java/com/example/flo/MainActivity.kt
+++ app/src/main/java/com/example/flo/MainActivity.kt
@@ -27,6 +27,7 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setTheme(R.style.Theme_FLO)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
