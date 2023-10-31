68fd55dc24708aa6001a83d8234f00794c300d2d
nahy-512 <hedghog1004@naver.com>
2023-11-01 01:48:59 +0900

    🎨 Style: Week6 - Song 클래스에 coverImg 추가, 전체 뷰 수정

--- app/src/main/java/com/example/flo/Song.kt
+++ app/src/main/java/com/example/flo/Song.kt
@@ -3,6 +3,7 @@ package com.example.flo
 data class Song(
     val title: String = "",
     val singer: String = "",
+    val coverImg: Int? = null,
     var second: Int = 0,
     var playTime: Int = 0,
     var isPlaying: Boolean = false,
