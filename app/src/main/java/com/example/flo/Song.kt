dce186b43d0d8dab86015b90753c656c16ec89a5
nahy-512 <hedghog1004@naver.com>
2023-11-06 00:41:24 +0900

    ✨ Feat: Week7 - RoomDB 세팅

--- app/src/main/java/com/example/flo/Song.kt
+++ app/src/main/java/com/example/flo/Song.kt
@@ -1,11 +1,18 @@
 package com.example.flo
 
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "SongTable")
 data class Song(
-    var title: String = "",
-    var singer: String = "",
-    var coverImg: Int? = null,
-    var second: Int = 0,
-    var playTime: Int = 0,
-    var isPlaying: Boolean = false,
-    var music: String = "" // 어떤 음악이 재생되고 있었는지 알려주는 변수
-)
+    var title: String = "", // 제목
+    var singer: String = "", // 가수
+    var coverImg: Int? = null, // 사진
+    var second: Int = 0, // 재생 시간
+    var playTime: Int = 0, // 현재 재생 시간
+    var isPlaying: Boolean = false, // 재생중인지
+    var music: String = "", // 어떤 음악이 재생되고 있었는지 알려주는 변수
+    var isLike: Boolean = false // 좋아요 여부
+) {
+    @PrimaryKey(autoGenerate = true) var id: Int = 0
+}
