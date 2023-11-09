f8888e038968f90b326a15d8fc35de62f444549c
Nahyun Kim <hedghog1004@naver.com>
2023-11-10 04:22:28 +0900

    🐛 Fix: 프래그먼트 생성자 이슈 해결

--- app/src/main/java/com/example/flo/BannerFragment.kt
+++ app/src/main/java/com/example/flo/BannerFragment.kt
@@ -7,9 +7,20 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.example.flo.databinding.FragmentBannerBinding
 
-class BannerFragment(val imgRes: Int) : Fragment() {
+class BannerFragment : Fragment() {
 
     lateinit var binding: FragmentBannerBinding
+
+    private var imgRes: Int = 0
+
+    companion object {
+        // 팩토리 메서드를 통해 새로운 프래그먼트 인스턴스를 생성
+        fun newInstance(imgRes: Int): BannerFragment {
+            val fragment = BannerFragment()
+            fragment.imgRes = imgRes
+            return fragment
+        }
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
--- app/src/main/java/com/example/flo/PanelFragment.kt
+++ app/src/main/java/com/example/flo/PanelFragment.kt
@@ -7,9 +7,20 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.example.flo.databinding.FragmentPanelBinding
 
-class PanelFragment(val imgRes: Int) : Fragment() {
+class PanelFragment : Fragment() {
 
     lateinit var binding : FragmentPanelBinding
+
+    private var imgRes: Int = 0
+
+    companion object {
+        // 팩토리 메서드를 통해 새로운 프래그먼트 인스턴스를 생성
+        fun newInstance(imgRes: Int): PanelFragment {
+            val fragment = PanelFragment()
+            fragment.imgRes = imgRes
+            return fragment
+        }
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
