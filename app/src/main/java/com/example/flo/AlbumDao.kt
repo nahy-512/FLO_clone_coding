70e92baff721763871f8396c881a82bbac7ae875
Nahyun Kim <hedghog1004@naver.com>
2023-11-12 04:18:21 +0900

    ✨ Feat: Week8 - 앨범 좋아요 기능 구현 및 DB 연결

--- app/src/main/java/com/example/flo/AlbumDao.kt
+++ app/src/main/java/com/example/flo/AlbumDao.kt
@@ -25,4 +25,19 @@ interface AlbumDao {
     @Query("SELECT * FROM AlbumTable")
     fun getAlbumsLiveData(): LiveData<List<Album>>
 
+    /* 좋아요 관련 */
+    @Insert
+    fun likeAlbum(like: Like)
+
+    // 사용자가 좋아요한 LikeTable의 아이디 반환
+    @Query("SELECT id FROM LikeTable WHERE userId = :userId AND albumId = :albumId")
+    fun isLikedAlbum(userId: Int, albumId: Int): Int?
+
+    // 좋아요 취소
+    @Query("DELETE FROM LikeTable WHERE userId = :userId AND albumID = :albumId")
+    fun dislikedAlbum(userId: Int, albumId: Int)
+
+    // 사용자가 좋아요한 앨범 목록 가져오기
+    @Query("SELECT AT.* FROM LikeTable as LT LEFT JOIN AlbumTable as AT ON LT.albumId = AT.id WHERE LT.userId = :userId")
+    fun getUserLikedAlbums(userId: Int): List<Album>
 }
