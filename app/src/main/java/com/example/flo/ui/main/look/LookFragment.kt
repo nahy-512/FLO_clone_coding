5faa792a94af3a3d85eb7ccb7351327ee304caf5
Nahyun Kim <hedghog1004@naver.com>
2024-01-06 03:44:34 +0900

    Feat: 둘러보기 탭 클릭 시 해당 위치로 스크롤하기 기능 구현

--- app/src/main/java/com/example/flo/ui/main/look/LookFragment.kt
+++ app/src/main/java/com/example/flo/ui/main/look/LookFragment.kt
@@ -1,13 +1,18 @@
 package com.example.flo.ui.main.look
 
+import android.content.res.ColorStateList
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ScrollView
+import android.widget.TextView
 import android.widget.Toast
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.flo.R
 import com.example.flo.ui.main.album.adapter.SongRVAdapter
 import com.example.flo.data.remote.song.FloChartResult
 import com.example.flo.data.remote.song.LookView
@@ -19,12 +24,20 @@ class LookFragment : Fragment(), LookView {
     private lateinit var binding: FragmentLookBinding
     private lateinit var floChartAdapter: SongRVAdapter
 
+    lateinit var scrollView : ScrollView
+
+    private lateinit var tabList: List<TextView>
+    private lateinit var titleList: List<TextView>
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         binding = FragmentLookBinding.inflate(inflater, container, false)
+
+        setInit()
+        setTabClickListeners()
 
         return binding.root
     }
@@ -33,6 +46,47 @@ class LookFragment : Fragment(), LookView {
         super.onStart()
 
         getSongs()
+    }
+
+    private fun setInit() {
+        binding.apply {
+            // 스크롤뷰 초기화
+            scrollView = lookSv
+            // 탭 리스트 초기화
+            tabList = listOf(
+                lookTabChart, lookTabVideo, lookTabGenre,
+                lookTabSituation, lookTabMood, lookTabAudio
+            )
+            // 타이틀 리스트 초기화
+            titleList = listOf(
+                lookChartTv, lookVideoTv, lookGenreTv,
+                lookSituationTv, lookMoodTv, lookAudioTv
+            )
+        }
+    }
+
+    private fun setTabClickListeners() {
+        for (i in tabList.indices) {
+            val button = tabList[i]
+
+            button.setOnClickListener {
+                initButton(i)
+            }
+        }
+    }
+
+    private fun initButton(idx : Int) {
+        for (presentTab: TextView in tabList) {
+            if(presentTab == tabList[idx]) { // 선택된 탭
+                presentTab.backgroundTintList = ColorStateList.valueOf(ContextCompat.getColor(requireContext(), R.color.select_color))
+                presentTab.setTextColor(ContextCompat.getColor(requireContext(), R.color.white))
+            } else { // 선택되지 않은 탭
+                presentTab.backgroundTintList = ColorStateList.valueOf(ContextCompat.getColor(requireContext(), R.color.light_gray_color))
+                presentTab.setTextColor(ContextCompat.getColor(requireContext(), R.color.dark_gray_color))
+            }
+        }
+        // 선택된 탭의 섹션 위치로 이동
+        scrollView.smoothScrollTo(0, titleList[idx].top)
     }
 
     private fun initRecyclerView(result: FloChartResult) {
